c41b0a8b0b0f4c4f6853b8fbe52a1bafa67fcaf5
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-08 15:52:38 -0300

    refactor: fix redundant atribute at class Timer

--- src/timer.hpp
+++ src/timer.hpp
@@ -2,7 +2,6 @@
 #define TIMER_HPP
 
 #include <chrono>
-using value_type = unsigned long long;
 using time_point = std::chrono::high_resolution_clock::time_point;
 using duration = std::chrono::duration<double>;
 
@@ -10,16 +9,14 @@ class Timer {
     private:
     time_point start_time;
     time_point end_time;
-    duration duration_time { end_time - start_time };
 
     public:
     Timer() = default;
 
-    void set_start_timer() { this->start_time = std::chrono::high_resolution_clock::now(); }
-    void set_end_timer() { this->end_time = std::chrono::high_resolution_clock::now(); }
-    duration get_duration_time() { return duration_time; }
+    void start_timer() { this->start_time = std::chrono::high_resolution_clock::now(); }
+    void end_timer() { this->end_time = std::chrono::high_resolution_clock::now(); }
     double total_time() { 
-        std::chrono::duration<double> diff = end_time - start_time;
+        duration diff = end_time - start_time;
         return diff.count(); 
     }
 };
