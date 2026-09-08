b94b0e9edc7277a31698dea7f6b0b0ca41db931e
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-08 18:57:40 -0300

    fix: added columns for csv file

--- include/csvparser.hpp
+++ include/csvparser.hpp
@@ -4,14 +4,19 @@
 #include <fstream>
 // #include <iomanip>
 #include <stdexcept>
+#include <filesystem>
 
 #include "Edge_Neighbor.hpp"
 
 struct CsvParser {
     static void save_csv(Dijkstra_Timer dij, Bellman_Ford_Timer bell, int size_n) {
 
+        bool file_exist = std::filesystem::exists("results.csv") && std::filesystem::file_size("results.csv") > 0;
         std::ofstream csv("results.csv", std::ios::app);
         if (csv.is_open()) {
+            if (!file_exist) {
+                csv << "dijkstra_time_ms,bellman_ford_time_ms,size_n\n"; // columns
+            }
             // csv << std::scientific << std::setprecision(2);
             csv << dij.d_timer.total_time() << "," // Djikstra time
             << bell.BF_timer.total_time() << "," // Bellman TIme
