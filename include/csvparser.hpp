bb35051b09bd35fc10d7e634784ab3ea17c8e52d
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-08 17:39:10 -0300

    fix: EOF on savecsv

--- include/csvparser.hpp
+++ include/csvparser.hpp
@@ -2,7 +2,7 @@
 #define CSVPARSER_HPP
 
 #include <fstream>
-#include <iomanip>
+// #include <iomanip>
 #include <stdexcept>
 
 #include "Edge_Neighbor.hpp"
@@ -12,10 +12,10 @@ struct CsvParser {
 
         std::ofstream csv("results.csv", std::ios::app);
         if (csv.is_open()) {
-            csv << std::scientific << std::setprecision(2);
+            // csv << std::scientific << std::setprecision(2);
             csv << dij.d_timer.total_time() << "," // Djikstra time
             << bell.BF_timer.total_time() << "," // Bellman TIme
-            << size_n << ","; // Size of n
+            << size_n << "\n"; // Size of n
             csv.close();
         } else {
             throw std::runtime_error("Erro: o arquivo csv não pôde ser aberto.");
