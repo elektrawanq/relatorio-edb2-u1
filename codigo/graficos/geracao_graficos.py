fd67427d70e5aa66554924831cc8aa196ca7edec
rterminus <luizrenanpontes@gmail.com>
2026-09-10 14:09:17 -0300

    ajuste de nomes de variaveis

--- codigo/graficos/geracao_graficos.py
+++ codigo/graficos/geracao_graficos.py
@@ -9,17 +9,19 @@ import pandas as pd
 df = pd.read_csv("results.csv")
 
 fig1, ax1 = plt.subplots()
-tempo_max = df["dijkstra_time_ms"].max()
-n_max_quadrado = (df["size_n"].max()) ** 2
-fator_ajuste = tempo_max / n_max_quadrado
-n_teorico = np.linspace(100, df["size_n"].max(), num=350)
-n_quadrado_teorico = (n_teorico**2) * fator_ajuste
+max_execution_time = df["dijkstra_time_ms"].max()
+max_n_squared = (df["size_n"].max()) ** 2
+adjust_factor = max_execution_time / max_n_squared
+theoric_n = np.linspace(100, df["size_n"].max(), num=350)
+theoric_n_squared = (theoric_n**2) * adjust_factor
 
-ax1.plot(df["size_n"], df["dijkstra_time_ms"], label="Dijkstra", marker="o")
-ax1.plot(df["size_n"], df["bellman_ford_time_ms"], label="Bellman-Ford", marker="s")
+ax1.plot(df["size_n"], df["dijkstra_time_ms"], label="Dijkstra Empírico", marker="o")
 ax1.plot(
-    n_teorico,
-    n_quadrado_teorico,
+    df["size_n"], df["bellman_ford_time_ms"], label="Bellman-Ford Empírico", marker="s"
+)
+ax1.plot(
+    theoric_n,
+    theoric_n_squared,
     label="$O(|V|^2)$ teórico",
     linestyle="--",
     color="gray",
@@ -34,14 +36,14 @@ fig1.savefig("grafico_tempos.pdf", bbox_inches="tight")
 plt.close(fig1)
 
 fig2, ax2 = plt.subplots()
-dijkstra_norm = df["dijkstra_time_ms"] / df["dijkstra_time_ms"].max()
-bellman_ford_norm = df["bellman_ford_time_ms"] / df["bellman_ford_time_ms"].max()
-n_teorico_norm = n_quadrado_teorico / n_quadrado_teorico.max()
+norm_dijkstra = df["dijkstra_time_ms"] / df["dijkstra_time_ms"].max()
+norm_bellman_ford = df["bellman_ford_time_ms"] / df["bellman_ford_time_ms"].max()
+norm_theoric_n = theoric_n_squared / theoric_n_squared.max()
 
-ax2.plot(df["size_n"], dijkstra_norm, label="Dijkstra Empírico", marker="o")
-ax2.plot(df["size_n"], bellman_ford_norm, label="Bellman-Ford Empírico", marker="s")
+ax2.plot(df["size_n"], norm_dijkstra, label="Dijkstra Normalizado", marker="o")
+ax2.plot(df["size_n"], norm_bellman_ford, label="Bellman-Ford Normalizado", marker="s")
 ax2.plot(
-    n_teorico, n_teorico_norm, label="$O(|V|^2)$ Teórico", linestyle="--", color="gray"
+    theoric_n, norm_theoric_n, label="$O(|V|^2)$ Teórico", linestyle="--", color="gray"
 )
 
 ax2.set(xlabel="Tamanho da Entrada (n)", ylabel="Tempo Normalizado (0 a 1)")
